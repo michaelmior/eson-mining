0776fe7d8cd85f5915b25cf6971f9cc07dbac051
Michael Mior <mmior@uwaterloo.ca>
2017-06-20 21:06:16 -0400

    Enable running TANE on RUBiS

--- build.sbt
+++ build.sbt
@@ -7,8 +7,9 @@ scalaVersion := "2.12.2"
 mainClass in Compile := Some("ca.uwaterloo.dsg.eson.TaneRunner")
 
 libraryDependencies ++= Seq(
-  "com.fasterxml.jackson.core" % "jackson-annotations" % "2.3.2",
-  "com.google.guava" % "guava" % "17.0",
-  "it.unimi.dsi" % "fastutil" % "6.5.9",
+  "com.fasterxml.jackson.core" % "jackson-annotations" % "2.8.9",
+  "com.google.guava" % "guava" % "19.0",
+  "it.unimi.dsi" % "fastutil" % "7.2.1",
+  "org.apache.calcite" % "calcite-cassandra" % "1.12.0",
   "org.apache.lucene" % "lucene-core" % "4.6.0"
 )
--- src/main/scala/ca/uwaterloo/dsg/eson/TaneRunner.scala
+++ src/main/scala/ca/uwaterloo/dsg/eson/TaneRunner.scala
@@ -16,13 +16,35 @@
  */
 package ca.uwaterloo.dsg.eson
 
+import de.metanome.backend.input.database.{DefaultDatabaseConnectionGenerator, DefaultTableInputGenerator}
+import de.metanome.algorithm_integration.configuration.{ConfigurationSettingDatabaseConnection, ConfigurationSettingTableInput, DbSystem}
 import de.metanome.algorithms.tane.TaneAlgorithm
+import java.sql.{Connection, DriverManager}
+import java.util.Properties
 
 object TaneRunner {
   def main(args: Array[String]): Unit = {
-    val tane = new TaneAlgorithm()
-    tane.setRelationalInputConfigurationValue(TaneAlgorithm.INPUT_TAG, new TestRelationalInputGenerator)
-    tane.setResultReceiver(new PrintingFunctionalDependencyReceiver)
-    tane.execute
+    val connString = "jdbc:calcite:model=src/main/resources/model.json"
+    val connectionProps = new Properties()
+    connectionProps.put("user", "admin")
+    connectionProps.put("password", "admin")
+    val conn = DriverManager.getConnection(connString, connectionProps)
+    val tables = conn.getMetaData().getTables(null, null, "%", null)
+    tables.next; tables.next // skip the header
+    val tableNames = new scala.collection.mutable.MutableList[String]
+    while (tables.next) {
+      tableNames += tables.getString(3)
+    }
+
+    val db = new ConfigurationSettingDatabaseConnection(connString, "admin", "admin", DbSystem.Oracle)
+
+    tableNames.foreach(tableName => {
+      val config = new ConfigurationSettingTableInput("\"" + tableName + "\"", db)
+      val table = new DefaultTableInputGenerator(config)
+      val tane = new TaneAlgorithm()
+      tane.setRelationalInputConfigurationValue(TaneAlgorithm.INPUT_TAG, table)
+      tane.setResultReceiver(new PrintingFunctionalDependencyReceiver)
+      tane.execute
+    })
   }
 }
